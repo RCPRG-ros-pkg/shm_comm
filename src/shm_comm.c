d333c0b8daa6528fe368d08d2444f6dd7a8eb497
Dawid Seredyński <dawid.seredynski@gmail.com>
2016-11-16 11:07:08 +0100

    robust mutex for each reader

--- src/shm_comm.c
+++ src/shm_comm.c
@@ -1,3 +1,30 @@
+/*
+ Copyright (c) 2014, Robot Control and Pattern Recognition Group, Warsaw University of Technology
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+     * Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+     * Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+     * Neither the name of the Warsaw University of Technology nor the
+       names of its contributors may be used to endorse or promote products
+       derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL <COPYright HOLDER> BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include "shm_comm.h"
 
 #include <stdlib.h>
@@ -42,12 +69,16 @@ int init_channel_hdr (int size, int readers, int flags, channel_hdr_t *shdata)
   shdata->readers = 0;
   shdata->max_readers = readers;
   shdata->size = size;
-  int* reader_ids = (int *) ((char *) shdata + sizeof(channel_hdr_t));
-  int* reading = (int *) ((char *) shdata + sizeof(channel_hdr_t) + readers * sizeof(int));
+  pthread_mutex_t* reader_ids = (pthread_mutex_t*) ((char *) shdata + sizeof(channel_hdr_t));
+  int* reading = (int *) ((char *) shdata + sizeof(channel_hdr_t) + readers * sizeof(pthread_mutex_t));
 
   for (size_t i = 0; i < readers; i++)
   {
-    reader_ids[i] = 0;
+    pthread_mutexattr_t mutexAttr;
+    pthread_mutexattr_init(&mutexAttr);
+    pthread_mutexattr_setpshared(&mutexAttr, PTHREAD_PROCESS_SHARED);
+    pthread_mutexattr_setrobust(&mutexAttr, PTHREAD_MUTEX_ROBUST);
+    pthread_mutex_init(&reader_ids[i], &mutexAttr);
     reading[i] = -1;
   }
 
@@ -73,7 +104,7 @@ int init_channel (channel_hdr_t* hdr, void* data, channel_t* chan)
 
   chan->hdr = hdr;
   chan->reader_ids = (int *) ((char*) hdr + sizeof(channel_hdr_t));
-  chan->reading = (int *) ((char *) hdr + sizeof(channel_hdr_t) + hdr->max_readers * sizeof(int));
+  chan->reading = (int *) ((char *) hdr + sizeof(channel_hdr_t) + hdr->max_readers * sizeof(pthread_mutex_t));
   chan->buffer = data;
   /*malloc ((hdr->max_readers + 2) * sizeof(char*));
   if (chan->buffer == NULL)
@@ -83,7 +114,7 @@ int init_channel (channel_hdr_t* hdr, void* data, channel_t* chan)
 
   for (size_t i = 0; i < (hdr->max_readers + 2); i++)
   {
-    chan->buffer[i] = ((char *) data) + hdr->max_readers * sizeof(int) + i * hdr->size;
+    chan->buffer[i] = ((char *) data) + hdr->max_readers * sizeof(pthread_mutex_t) + i * hdr->size;
   }*/
   return 0;
 }
@@ -225,16 +256,66 @@ int create_reader (channel_t* chan, reader_t* reader)
 
     for (int i = 0; i < chan->hdr->max_readers; i++)
     {
-      if (chan->reader_ids[i] == 0)
+      int lock_status = pthread_mutex_trylock(&chan->reader_ids[i]);
+      int acquired = FALSE;
+      switch (lock_status)
       {
-        chan->reader_ids[i] = 1;
+      case 0:
+        acquired = TRUE;
+        break;
+      case EBUSY:
+        // do nothing:
+        // other reader acquired the mutex
+        // the loop should be continued
+        break;
+      case EINVAL:
+        err = -5;
+        break;
+      case EAGAIN:
+        err = -6;
+        break;
+      case EDEADLK:
+        err = -7;
+        break;
+      case EOWNERDEAD:
+        // the reader that acquired the mutex is dead
+        //TODO: make the state consistent
+
+        // recover the mutex
+        if (pthread_mutex_consistent(&chan->reader_ids[i]) == EINVAL) {
+          err = -8;
+          break;
+        }
+        if (pthread_mutex_unlock(&chan->reader_ids[i]) != 0) {
+          err = -9;
+          break;
+        }
+        if (pthread_mutex_trylock(&chan->reader_ids[i]) != 0) {
+          err = -10;
+          break;
+        }
+        chan->hdr->readers--;
+        acquired = TRUE;
+        break;
+      default:
+        // other error
+        err = -11;
+        break;
+      }
+
+      if (err != 0) {   // an error occured in current iteration
+        break;
+      }
+
+      if (acquired)
+      {
         reader->id = i;
         chan->hdr->readers++;
         break;
       }
     }
 
-    if (reader->id < 0)
+    if (reader->id < 0 && err == 0)
     {
       err = -3;
     }
@@ -258,7 +339,7 @@ void release_reader (reader_t* re)
 
   pthread_mutex_lock (&re->channel->hdr->mtx);
 
-  re->channel->reader_ids[re->id] = 0;
+  pthread_mutex_unlock(&re->channel->reader_ids[re->id]);
   re->channel->hdr->readers--;
   re->id = -1;
 
--- src/shm_comm.h
+++ src/shm_comm.h
@@ -1,3 +1,30 @@
+/*
+ Copyright (c) 2014, Robot Control and Pattern Recognition Group, Warsaw University of Technology
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+     * Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+     * Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+     * Neither the name of the Warsaw University of Technology nor the
+       names of its contributors may be used to endorse or promote products
+       derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL <COPYright HOLDER> BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #ifndef __SHM_COMM__
 #define __SHM_COMM__
 
@@ -7,6 +34,10 @@
  */
 
 #include <pthread.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #define FALSE 0
 #define TRUE 1
@@ -35,7 +66,7 @@ typedef struct {
 
 typedef struct {
     channel_hdr_t *hdr;
-    int *reader_ids; //! contain information which reader ids are in use
+    pthread_mutex_t *reader_ids; //! contain information which reader ids are in use
     int *reading; //! index to currently used buffers
     char *buffer; //! data buffer ptr
 } channel_t;
@@ -86,5 +117,9 @@ int reader_buffer_get(reader_t* re, void** buf);
 int reader_buffer_wait(reader_t* re, void** buf);
 
 int reader_buffer_timedwait(reader_t* re, const struct timespec *abstime, void** buf);
+
+#ifdef __cplusplus
+};  // extern "C"
+#endif
 
 #endif // __SHM_COMM__
